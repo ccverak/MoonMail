8d90793aa2e4254d8a7f908a62f37b6d99a03323
Carlos Castellanos Vera <ccverak@gmail.com>
2018-04-02 18:01:45 +0200

    WIP

--- emails-microservice/src/jobs/index.js
+++ emails-microservice/src/jobs/index.js
@@ -2,6 +2,7 @@ import Promise from 'bluebird';
 import moment from 'moment';
 import uuidv4 from 'uuid/v4';
 import { BaseModel } from 'moonmail-models';
+import { exec } from 'child_process';
 
 class ScheduledJob extends BaseModel {
   static tableName() {
@@ -16,7 +17,10 @@ class ScheduledJob extends BaseModel {
 
 class Worker {
   static perform(payload, performIn) {
-    return schedule(this.prototype.constructor.name, payload, performIn);
+    // from serverless conventions
+
+    const subscriber = this.prototype.constructor.name;
+    return schedule(subscriber, subscriberFingerprint, payload, performIn);
   }
 }
 
@@ -24,17 +28,34 @@ const schedule = function schedule(subject, payload = {}, performIn = moment.dur
   return ScheduledJob.create({ id: uuidv4(), subject, payload, scheduledAt: moment().add(performIn).unix() });
 };
 
+const executionStrategy = {
+  local: ({ subject, payload }) => {
+    const Ctor = eval(subject);
+    return new Ctor().perform(payload);
+  },
+  lambda: ({ subject, payload }) => {
+    const resourceName = `${process.env.SERVICE_NAME}-${process.env.SERVICE_STAGE}-${subject}`;
+    // call a lambda
+  },
+  sqs: ({ subject, payload }) => {
+
+  },
+  sns: ({ subject, payload }) => {
+
+  }
+};
 const execute = function execute({ subject, payload }) {
-  const Ctor = eval(subject);
-  return new Ctor().perform(payload);
+  // build the lambda function name
+  return executionStrategy.lambda({ subject, payload });
 };
 
 const executeJobs = function executeJobs() {
   return ScheduledJob._client.scan().promise()
     .then(jobs => Promise.map(jobs, (job) => {
       if (job.scheduledAt > moment().unix()) return Promise.resolve();
-      return execute({ subject: job.subject, payload: job.payload })
-        .then(ScheduledJob.delete(job.id));
+      return execute({ subject: job.subject, payload: job.payload });
+      // updating jobs statuses should be included in the
+      // base worker class
     }));
 };
 
